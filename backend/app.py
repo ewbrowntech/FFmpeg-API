c423d9638e68832202554038c8e03ca6cb893a26
Ethan Brown <ethan@ewbrowntech.com>
2024-02-15 23:48:00 -0600

    Added POST /transcode and POST /merge

--- backend/app.py
+++ backend/app.py
@@ -18,3 +18,19 @@ app = FastAPI()
 @app.get("/")
 async def hello_world():
     return "Hello, world!"
+
+
+@app.post("/transcode")
+async def transcode():
+    """
+    Transcode an audio or video stream
+    """
+    return "Transcode"
+
+
+@app.post("/merge")
+async def merge():
+    """
+    Merge an audio and video stream into one multimedia stream
+    """
+    return "Merge"
